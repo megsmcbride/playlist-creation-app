774fa4d9ac50ce19920cb503bdeeafa0d12cd470
Megan McBride <meganambermcbride@gmail.com>
2022-11-03 11:11:15 -0400

    Add spotify authentication with login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import '../style/Login.css'
 
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=a061f41543a94d3c975ea7336dec788d&response_type=code&redirect_uri=http://localhost:3000/callback&scope=ugc-image-upload%20user-modify-playback-state%20user-read-playback-state%20user-read-currently-playing%20playlist-read-collaborative%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20app-remote-control%20streaming%20user-read-email%20user-read-private%20user-library-modify%20user-library-read"
+  "https://accounts.spotify.com/authorize?client_id=b824eb11a849405da56c45f7bf05ff50&response_type=code&redirect_uri=http://localhost:3000/callback&scope=ugc-image-upload%20user-modify-playback-state%20user-read-playback-state%20user-read-currently-playing%20playlist-read-collaborative%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20app-remote-control%20streaming%20user-read-email%20user-read-private%20user-library-modify%20user-library-read"
 
   export default function Login() {
   return (
