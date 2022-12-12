343227d31390d195844d706c95c265aa3d949473
Megan McBride <meganambermcbride@gmail.com>
2022-12-12 08:46:43 -0500

    Add conditional rendering and retrieve login authentication code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import Login from './components/Login';
 import './App.css';
 
+const code = new URLSearchParams(window.location.search).get("code");
+console.log("here", code)
+
 function App() {
+  const userID = JSON.parse(localStorage.getItem("userID"));
   return (
     <div className="App">
       <h2 className='header'>collabify</h2>
+      {code ? 
       <div className='intro'>
 
       <h4>Let's get started</h4>
       <p className='intro-message'>Sign in to create your own custom playlist</p>
      <Login/>
-      </div>
+      </div> : <div> hi </div> }
 
  
     </div>
