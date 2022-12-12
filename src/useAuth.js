1a1adb538e21fe2ee084cbbb559aea235a814fb1
Megan McBride <meganambermcbride@gmail.com>
2022-12-12 08:47:56 -0500

    Add authentication to refresh token

---
+++ src/useAuth.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useAuth(code) {
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
+  const [username, setUsername] = useState();
+  const [userID, setUserID] = useState();
+
+  useEffect(() => {
+    if (code) {
+      axios
+        .post("http://localhost:3001/login", { code })
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          localStorage.setItem(
+            "access_token",
+            JSON.stringify(res.data.accessToken)
+          );
+          setRefreshToken(res.data.refreshToken);
+          setExpiresIn(res.data.expiresIn);
+          setUsername(res.data.name);
+          localStorage.setItem("username", JSON.stringify(res.data.name));
+          setUserID(res.data.userID);
+          localStorage.setItem("userID", JSON.stringify(res.data.userID));
+          window.history.pushState({}, null, "/");
+        })
+        .catch(() => {});
+    }
+  }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:3001/refresh", {
+          refreshToken,
+        })
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          localStorage.setItem(
+            "access_token",
+            JSON.stringify(res.data.accessToken)
+          );
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, (expiresIn - 60) * 1000);
+
+    return () => clearInterval(interval);
+  }, [refreshToken, expiresIn]);
+
+  return { accessToken, setAccessToken, refreshToken, username, userID };
+}
