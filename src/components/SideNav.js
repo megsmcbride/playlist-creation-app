6309201167958e1e67c4a7b5be90c31110038a3c
Megan McBride <meganambermcbride@gmail.com>
2022-12-12 09:23:28 -0500

    Add header

---
+++ src/components/SideNav.js
@@ -0,0 +1,13 @@
+import '../style/SideNav.css';
+
+export default function SideNav() {
+  return (
+    <div className='sidenav-container'>
+      <div>
+
+      <h2 className='sidenav-header'>collabify</h2>
+      </div>
+
+    </div>
+  );
+}
