51ccee12f3a07d6f0a95237aed92286b10c0971c
Megan McBride <meganambermcbride@gmail.com>
2022-12-19 14:40:48 -0500

    Fix header location

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import Login from './components/Login';
-import './App.css';
-import SideNav from './components/SideNav';
+import Login from "./components/Login";
+import "./App.css";
+import SideNav from "./components/SideNav";
 
 const code = new URLSearchParams(window.location.search).get("code");
-console.log("here", code)
+console.log("here", code);
 
 function App() {
   const userID = JSON.parse(localStorage.getItem("userID"));
   return (
     <div className="App">
-      {code ? <SideNav code = {code}/> :
-      <div className='intro'>
-        <h2 className='header'>collabify</h2>
-
-      <h4>Let's get started</h4>
-      <p className='intro-message'>Sign in to create your own custom playlist</p>
-     <Login/>
-      </div> }
-
- 
+      {code ? (
+        <SideNav code={code} />
+      ) : (
+        <div>
+          <h2 className="header">collabify</h2>
+          <div className="intro">
+            <h4>Let's get started</h4>
+            <p className="intro-message">
+              Sign in to create your own custom playlist
+            </p>
+          <Login />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
